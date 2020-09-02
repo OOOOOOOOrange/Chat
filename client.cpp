9370fa61507d259ad9de1500e8704c8e40db5b97
Orange <49386018+OOOOOOOOrange@users.noreply.github.com>
2020-09-02 15:06:46 +0800

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -40,7 +40,7 @@ int main (int argc, const char * argv[])
             cout<<"用户不存在！"<<endl;
         }
     }
-    
+    //设置socket
     struct sockaddr_in server_addr;
     server_addr.sin_len = sizeof(struct sockaddr_in);
     server_addr.sin_family = AF_INET;
@@ -53,27 +53,31 @@ int main (int argc, const char * argv[])
         perror("socket error");
         return 1;
     }
-    char recv_msg[BUFFER_SIZE];
-    char input_msg[BUFFER_SIZE];
-
+    char recv_msg[BUFFER_SIZE];//收信
+    char input_msg[BUFFER_SIZE];//发信
+    //尝试连接
     if (connect(server_sock_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in))==0) {
+        //.创建一个fd_set变量（fd_set实为包含了一个整数数组的结构体），用来存放所有的待检查的文件描述符
        fd_set client_fd_set;
+        //等待时间
        struct timeval tv;
        tv.tv_sec = 20;
        tv.tv_usec = 0;
 
 
        while (1) {
+           //清空fd_set变量，并将需要检查的所有文件描述符加入fd_set
            FD_ZERO(&client_fd_set);
            FD_SET(STDIN_FILENO, &client_fd_set);
            FD_SET(server_sock_fd, &client_fd_set);
 
-           int ret = select(server_sock_fd + 1, &client_fd_set, NULL, NULL, &tv);
+           int ret = select(server_sock_fd + 1, &client_fd_set, NULL, NULL, &tv);//调用select。若返回-1，则说明出错;返回0,则说明超时，返回正数，则为发生状态变化的文件描述符的个数
            if (ret < 0 ) {//出错
                continue;
            }else if(ret ==0){//超时
                continue;
            }else{
+               //发送消息
                if (FD_ISSET(STDIN_FILENO, &client_fd_set)) {
                    bzero(input_msg, BUFFER_SIZE);
                    fgets(input_msg, BUFFER_SIZE, stdin);
@@ -84,7 +88,7 @@ int main (int argc, const char * argv[])
                        }
                    }
                }
-
+               //接收消息
                if (FD_ISSET(server_sock_fd, &client_fd_set)) {
                    bzero(recv_msg, BUFFER_SIZE);
                    long byte_num = recv(server_sock_fd,recv_msg,BUFFER_SIZE,0);
